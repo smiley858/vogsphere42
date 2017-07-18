7cab0e67902e9fc9867e55abdcbe801bbdad0751
gchojnac <gchojnac@student.42.fr>
2017-07-18 11:12:14 +0200

    ex01 & ex02 - print_name & print_params - done

---
+++ day06/ex01/ft_print_program_name.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gchojnac <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/18 11:03:07 by gchojnac          #+#    #+#             */
+/*   Updated: 2017/07/18 11:09:17 by gchojnac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	while (*str != 0)
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	(void)argc;
+	ft_putstr(argv[0]);
+	ft_putchar('\n');
+	return (0);
+}
---
+++ day06/ex02/ft_print_params.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gchojnac <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/18 11:07:39 by gchojnac          #+#    #+#             */
+/*   Updated: 2017/07/18 11:09:47 by gchojnac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	while (*str != 0)
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	int	index;
+
+	index = 1;
+	while (index < argc)
+	{
+		ft_putstr(argv[index]);
+		ft_putchar('\n');
+		index++;
+	}
+	return (0);
+}
