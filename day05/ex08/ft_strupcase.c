3cdbc380fd010963bc0f0a9adb2d6a8788de7e4e
gchojnac <gchojnac@student.42.fr>
2017-07-26 11:14:20 +0200

    adding j05 ++

--- day05/ex08/ft_strupcase.c
+++ day05/ex08/ft_strupcase.c
@@ -1,30 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*   ft_strupcase.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gchojnac <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/07/12 21:41:45 by gchojnac          #+#    #+#             */
-/*   Updated: 2017/07/12 22:23:40 by gchojnac         ###   ########.fr       */
+/*   Created: 2017/07/16 22:06:55 by gchojnac          #+#    #+#             */
+/*   Updated: 2017/07/17 14:45:46 by gchojnac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strstr(char *str, char *to_find)
+char	*ft_strupcase(char *str)
 {
-	char	*toreturn;
-	char	*mem_find;
+	int	index;
 
-	mem_find = to_find;
-	if (*to_find == 0)
-		return *str;
-	while (*str != 0)
+	index = 0;
+	while (str[index] != 0)
 	{
-		if (*str == *to_find)
-		{
-			*to_find++;	
-		}
-		to_find
-		str++;
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] = str[index] - 32;
+		index++;
 	}
+	return (str);
 }
